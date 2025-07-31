988f1497d4ddd37ec56bd3096afa1fd71fd8d3ad
stevenwang2 <stevenwang2@macbookpro.lan>
2025-07-31 12:00:18 -0400

    default to duckdb in example

--- hyperliquid_pipeline.py
+++ hyperliquid_pipeline.py
@@ -9,27 +9,29 @@ def load_hyperliquid() -> None:
     # Load environment variables from .env file
     load_dotenv()
 
-    # pipeline = dlt.pipeline(
-    #     pipeline_name="funding_rates",
-    #     destination='duckdb',
-    #     dataset_name="hyperliquid",
-    # )
-
-    db = postgres(
-        credentials=(
-            f"postgresql://"
-            f"{os.getenv('POSTGRES_USER')}:"
-            f"{os.getenv('POSTGRES_PASSWORD')}@"
-            f"{os.getenv('POSTGRES_HOST')}:"
-            f"{os.getenv('POSTGRES_PORT')}/"
-            f"{os.getenv('POSTGRES_DATABASE')}"
-        )
-    )
+    # DUCKDB pipeline
     pipeline = dlt.pipeline(
         pipeline_name="funding_rates",
-        destination=db,
+        destination='duckdb',
         dataset_name="hyperliquid",
     )
+
+    # POSTGRES pipeline
+    # db = postgres(
+    #     credentials=(
+    #         f"postgresql://"
+    #         f"{os.getenv('POSTGRES_USER')}:"
+    #         f"{os.getenv('POSTGRES_PASSWORD')}@"
+    #         f"{os.getenv('POSTGRES_HOST')}:"
+    #         f"{os.getenv('POSTGRES_PORT')}/"
+    #         f"{os.getenv('POSTGRES_DATABASE')}"
+    #     )
+    # )
+    # pipeline = dlt.pipeline(
+    #     pipeline_name="funding_rates",
+    #     destination=db,
+    #     dataset_name="hyperliquid",
+    # )
 
     load_info = pipeline.run(hyperliquid_source())
     print(load_info)  # noqa: T201
